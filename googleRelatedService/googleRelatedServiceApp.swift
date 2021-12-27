7c89456de23603627e97d05bd4e46dc1148305e9
leoChang <leochang@gmail.com>
2021-12-27 16:15:22 +0800

    Initial Commit

---
+++ googleRelatedService/googleRelatedServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  googleRelatedServiceApp.swift
+//  googleRelatedService
+//
+//  Created by User16 on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct googleRelatedServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
